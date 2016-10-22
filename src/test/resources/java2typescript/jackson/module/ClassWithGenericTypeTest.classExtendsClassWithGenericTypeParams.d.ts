a65de0eea2b8128ebf6ccd9de7a8d747f1605593
Ats Uiboupin <ats.uiboupin@iglu.ee>
2016-10-22 16:07:27 +0300

    test(output check): ignore comments in test expected output file

--- src/test/resources/java2typescript/jackson/module/ClassWithGenericTypeTest.classExtendsClassWithGenericTypeParams.d.ts
+++ src/test/resources/java2typescript/jackson/module/ClassWithGenericTypeTest.classExtendsClassWithGenericTypeParams.d.ts
@@ -1,3 +1,7 @@
+/**
+ * At the moment superclasses of Java class are not reflected by TS interface,
+ * fields from superclass GenericClass<String> are inlined
+ */
 export interface StringClass {
     someField: string;
     genericList: string[];
--- src/test/resources/java2typescript/jackson/module/ClassWithGenericTypeTest.classWithCollections.d.ts
+++ src/test/resources/java2typescript/jackson/module/ClassWithGenericTypeTest.classWithCollections.d.ts
@@ -1,4 +1,5 @@
 export interface ClassWithCollections {
+    // collections (List<T> and Collection<T>) are converted into arrays (T[])
     stringList: string[];
     booleanCollection: boolean[];
     booleanWrapperArray: boolean[];
